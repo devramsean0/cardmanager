fde32c70b99bde438aced2107b18c5ffe5460e57
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2024-06-22 15:24:37 +0100

    Dump Downloading!!!

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,16 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import * as FileSystem from 'expo-file-system';
+import { FSDirs } from '@/constants/FSDirs';
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -15,6 +21,7 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  const [downloading, setDownloading] = useState(true);
 
   useEffect(() => {
     if (loaded) {
@@ -24,6 +31,56 @@ export default function RootLayout() {
 
   if (!loaded) {
     return null;
+  }
+  useEffect(() => {
+    const downloadLocation = `${FSDirs.dataDump}/data.json`;
+    const checkIfFileExists = async () => {
+      try {
+        const file = await FileSystem.getInfoAsync(downloadLocation);
+        if (file.exists) {
+          console.log('Data file exists')
+          setDownloading(false);
+          return true;
+        } else {
+          return false;
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    const downloadFile = async () => {
+      try {
+        const BulkDataFileRes = await fetch(`https://api.scryfall.com/bulk-data/oracle-cards`);
+        const BulkDataFileData = await BulkDataFileRes.json();
+        const file = await FileSystem.downloadAsync(
+          BulkDataFileData.download_uri,
+          downloadLocation
+        );
+        if (file.status === 200) {
+          console.log('Data File downloaded');
+          setDownloading(false);
+        } else {
+          console.log('Failed to download file');
+          // TODO: Report this to user/US
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    checkIfFileExists().then((exists) => {
+      if (!exists) {
+        downloadFile();
+      }
+    });
+  }, [downloading]);
+  if (downloading) {
+    return (
+      <SafeAreaView>
+        <View>
+          <Text className="flex flex-row justify-center">Downloading...</Text>
+        </View>
+      </SafeAreaView>
+    )
   }
 
   return (
---
+++ constants/FSDirs.ts
@@ -0,0 +1,5 @@
+import { cacheDirectory } from "expo-file-system"
+
+export const FSDirs = {
+    "dataDump": cacheDirectory,
+}
